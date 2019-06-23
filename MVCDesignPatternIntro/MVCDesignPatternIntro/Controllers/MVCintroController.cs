cb1c564830aad7c70b102f597643e1b1b471c484
Sree <mulelearner1234@gmail.com>
2019-06-23 19:26:42 -0400

    First Commit

---
+++ MVCDesignPatternIntro/MVCDesignPatternIntro/Controllers/MVCintroController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDesignPatternIntro.Models;
+
+namespace MVCDesignPatternIntro.Controllers
+{
+    public class MVCintroController : Controller
+    {
+        //
+        // GET: /MVCintro/
+       [HttpGet]
+        public ActionResult Index()
+        {
+            string UserID = Request.QueryString["userid"];
+
+          UserData objUser = new UserData(Convert.ToInt32 (UserID));
+          ViewBag.LastName = objUser.LastName;
+           
+           ViewBag.UserID = UserID;
+ 
+           return View();
+        }
+       [HttpPost]
+       public ActionResult Index(FormCollection myCollection)
+       {
+           string LastName = myCollection["txtLastName"];
+           string btnSubmitValue = myCollection["btnSubmit"];
+           string btnDeleteValue = myCollection["btnDelete"];
+           
+           int UserID = Convert.ToInt16 (myCollection["UserID"]);
+
+           if (btnSubmitValue == "btnSubmit")
+           {        // some operation or alternative view()
+           }
+           else if (btnDeleteValue == "btnDelete")
+           {        // some operation or alternative view()
+           }
+
+            return View();
+       }
+
+    }
+}
---
+++ MVCDesignPatternIntro/MVCDesignPatternIntro/Models/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDesignPatternIntro.Models
+{
+    public class UserData
+    {
+
+        public UserData(Int32 UserID)
+        {
+
+        }
+
+        private string pLastName = "Jerry";
+
+        public string LastName 
+        {
+            get { return pLastName;}
+            set { pLastName= value;}
+        }
+
+    }
+}
